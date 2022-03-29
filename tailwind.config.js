32304d6e42502be562f365f6db751f5af71d6e98
ArJit0009 <arjitg36@gmaail.com>
2022-03-29 16:22:59 +0530

    scrollbar added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
 module.exports = {
   content: [ './src/**/*.{html,js}','./public/**/*.{html,js}',],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+
+        center: true,
+  
+        padding: '16px',
+  
+        screens: {
+  
+          lg: "1440px",
+  
+          xl: "1440px",
+  
+          '2xl': "1440px",
+  
+        },
+    },
   },
   plugins: [],
+}
 }
